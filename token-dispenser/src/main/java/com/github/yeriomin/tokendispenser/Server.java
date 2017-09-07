4739daa6a4086b72f153718a496248c2b1950da0
4knahs <4knahs@gmail.com>
2017-09-07 14:02:37 +0200

    enabling port setting

--- token-dispenser/src/main/java/com/github/yeriomin/tokendispenser/Server.java
+++ token-dispenser/src/main/java/com/github/yeriomin/tokendispenser/Server.java
@@ -41,7 +41,7 @@ public class Server {
 
     public static void main(String[] args) {
         String host = "0.0.0.0";
-        int port = 8080;
+        int port = Integer.valueOf(System.getenv("DISPENSER_PORT"));
         ipAddress(host);
         port(port);
         notFound("Not found");
